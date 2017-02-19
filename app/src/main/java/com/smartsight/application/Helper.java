5f0bffdcd6eb7a08c046ca6afb784684924f8078
bertrandda <bertrand.daure@laposte.net>
2017-02-19 22:30:51 +0100

    Add config file

---
+++ app/src/main/java/com/smartsight/application/Helper.java
@@ -0,0 +1,33 @@
+package com.smartsight.application;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public final class Helper {
+
+    private static final String TAG = "HELPER";
+
+    public static String getConfigValue(Context context, String name) {
+        Resources resources = context.getResources();
+
+        try {
+            InputStream rawResource = resources.openRawResource(R.raw.config);
+            Properties properties = new Properties();
+            properties.load(rawResource);
+            return properties.getProperty(name);
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Unable to find the config file: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to open config file.");
+        }
+
+        return null;
+    }
+}
