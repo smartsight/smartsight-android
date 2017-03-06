63c0044f84b3998019d59499baa180a689566401
francoischalifour <francois.chalifour@gmail.com>
2017-03-06 16:10:33 +0100

    Add length param to the showToast util

--- app/src/main/java/com/smartsight/app/util/ViewUtil.kt
+++ app/src/main/java/com/smartsight/app/util/ViewUtil.kt
@@ -3,5 +3,5 @@ package com.smartsight.app.util
 import android.content.Context
 import android.widget.Toast
 
-fun showToast(context: Context, resId: Int) =
-        Toast.makeText(context, resId, Toast.LENGTH_SHORT).show()
+fun showToast(context: Context, resId: Int, length: Int = Toast.LENGTH_SHORT) =
+        Toast.makeText(context, resId, length).show()
